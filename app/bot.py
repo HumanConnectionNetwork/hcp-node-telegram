6dc5593a67cceb5cf3e8c6e78ba2cda4e167318d
Human Connection Foundation <reddeconexionhumana@gmail.com>
2026-07-06 16:33:42 -0300

    Add handler for reported name in bot

--- app/bot.py
+++ app/bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler
 
 from app.config import settings
-from app.conversation.create_record import create_record_menu
+from app.conversation.create_record import create_record_menu, ask_reported_name
 from app.conversation.start import start
 
 
@@ -14,6 +14,10 @@ def main() -> None:
 
     application.add_handler(
         CallbackQueryHandler(create_record_menu, pattern="^create_report$")
+    )
+
+    application.add_handler(
+        CallbackQueryHandler(ask_reported_name, pattern="^event_")
     )
 
     print("HCP Telegram Client is running...")
